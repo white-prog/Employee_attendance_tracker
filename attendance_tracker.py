a1e55b125deaecf029d3a6254d0bba95df6a7ba8
white-prog <sabinhameed55@gmail.com>
2025-01-01 23:51:57 +0530

    attendance tracker before debug

---
+++ attendance_tracker.py
@@ -0,0 +1,35 @@
+
+def main():
+    from datetime import date
+    Attendance = []
+    print("Employee Attendance Tracker")
+    print("1. Mark Attendance")
+    print("2. View Attendance")
+    print("3. Exit")
+    for i in range(3):
+        print("")
+    while True:
+        inp = input("Choose an option: ")
+        if inp == 1:
+            name = input("Enter employee name: ")
+            attendan = input(f"Is {name} present or absent? ")
+            print(f"Attendance for {name} has been recorded.")
+            Attendance.append((name,attendan))
+        elif inp == 2:
+            tdy = date.today()
+            print(f"Today's attendance - ({str(tdy)})")
+            for attend in Attendance:
+                print(f"- {attend[0]} : {attend[1]}")
+        elif inp == 3:
+            print("Goodbye!")
+            break
+        else:
+            print("Wrong input!")
+
+if __name__ == "__main__":
+    main()
+            
+
+
+
+
